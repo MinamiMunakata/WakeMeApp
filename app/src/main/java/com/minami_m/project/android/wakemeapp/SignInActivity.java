e9e7ed4c45ab6b8136037c812875e9eef5c336ae
Minamiciccc <june28munazo@gmail.com>
2018-09-30 13:16:29 -0700

    create the launching app page

--- app/src/main/java/com/minami_m/project/android/wakemeapp/SignInActivity.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/SignInActivity.java
@@ -1,6 +1,7 @@
 package com.minami_m.project.android.wakemeapp;
 
 import android.content.Intent;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -22,6 +23,25 @@ public class SignInActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.minami_m.project.android.wakemeapp.R.layout.activity_signin);
+
+        // Check that the activity is using the layout version with
+        // the signin_container FrameLayout
+        if (findViewById(R.id.signin_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            LaunchingAppFragment launchingAppFragment = new LaunchingAppFragment();
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.signin_container, launchingAppFragment).commit();
+        }
 
         // Choose authentication providers
         List<AuthUI.IdpConfig> providers = Arrays.asList(
@@ -48,7 +68,26 @@ public class SignInActivity extends AppCompatActivity {
             if (resultCode == RESULT_OK) {
                 // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                // ...
+
+                // TODO: Check if the user is new
+                // TODO: Show the sign up page and create an account
+
+                // Change the UI
+                // Create fragment and give it an argument specifying the article it should show
+                ProfileFragment profileFragment = new ProfileFragment();
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+
+                // Replace whatever is in the fragment_container view with this fragment,
+                // and add the transaction to the back stack so the user can navigate back
+                transaction.replace(R.id.signin_container, profileFragment);
+                // TODO: Remove addToBackStack()
+                transaction.addToBackStack(null);
+
+                // Commit the transaction
+                transaction.commit();
+
+                // TODO: If the user already has an account, jump to a dialog page
+
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
