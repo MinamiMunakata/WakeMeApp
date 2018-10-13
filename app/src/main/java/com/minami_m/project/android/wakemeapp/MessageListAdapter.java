0b170e848cfef9cfdef12a65eee712ada9a049bd
Minami Munakata <june28munazo@gmail.com>
2018-10-12 18:48:29 -0700

    fixed color value

--- app/src/main/java/com/minami_m/project/android/wakemeapp/MessageListAdapter.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/MessageListAdapter.java
@@ -1,6 +1,27 @@
 package com.minami_m.project.android.wakemeapp;
 
+import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
 
 public class MessageListAdapter extends RecyclerView.Adapter {
+    private static final int VIEW_TYPE_MESSAGE_SENT = 1;
+    private static final int VIEW_TYPE_MESSAGE_RECEIVED = 2;
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 }
