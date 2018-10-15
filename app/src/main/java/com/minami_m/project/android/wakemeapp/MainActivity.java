b90e54015c514cfe451f5e4e44ba8f58ec036bc9
Minami Munakata <june28munazo@gmail.com>
2018-10-14 18:06:32 -0700

    set function on semicircle button

--- app/src/main/java/com/minami_m/project/android/wakemeapp/MainActivity.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.minami_m.project.android.wakemeapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ActivityChangeListener{
+    ImageButton button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = findViewById(R.id.semicircle_btn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                launchActivity(SearchFriendActivity.class);
+            }
+        });
+    }
+
+    @Override
+    public void launchActivity(Class nextActivity) {
+        Intent intent = new Intent(this, nextActivity);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/minami_m/project/android/wakemeapp/SearchFriendActivity.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/SearchFriendActivity.java
@@ -1,13 +1,19 @@
 package com.minami_m.project.android.wakemeapp;
 
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SearchFriendActivity extends AppCompatActivity {
+public class SearchFriendActivity extends AppCompatActivity implements FragmentChangeListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search_friend);
+    }
+
+    @Override
+    public void replaceFragment(Fragment newFragment) {
+
     }
 }
