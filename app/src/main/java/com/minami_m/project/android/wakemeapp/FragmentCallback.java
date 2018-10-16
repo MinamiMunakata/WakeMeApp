2657279e820570550d56d5a1594a7d84845a3434
Minami Munakata <june28munazo@gmail.com>
2018-10-16 16:18:45 -0700

    search result view

---
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/FragmentCallback.java
@@ -0,0 +1,4 @@
+package com.minami_m.project.android.wakemeapp;
+
+public interface FragmentCallback {
+}
--- app/src/main/java/com/minami_m/project/android/wakemeapp/SearchFriendResultFragment.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/SearchFriendResultFragment.java
@@ -11,10 +11,10 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFriendButtonFragment extends Fragment {
+public class SearchFriendResultFragment extends Fragment {
 
 
-    public SearchFriendButtonFragment() {
+    public SearchFriendResultFragment() {
         // Required empty public constructor
     }
 
@@ -23,7 +23,7 @@ public class SearchFriendButtonFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search_friend_button, container, false);
+        return inflater.inflate(R.layout.fragment_search_friend_result, container, false);
     }
 
 }
