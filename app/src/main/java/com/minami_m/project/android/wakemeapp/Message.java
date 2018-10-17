6c3dd5cc286db7eb09d520b5c9e0b12c04bfc05d
Minami Munakata <june28munazo@gmail.com>
2018-10-17 16:11:43 -0700

    create a message object

--- app/src/main/java/com/minami_m/project/android/wakemeapp/Message.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/Message.java
@@ -1,4 +1,11 @@
 package com.minami_m.project.android.wakemeapp;
 
+import java.util.Date;
+
 public class Message {
+    private String id;
+    private String text;
+    private String autor;
+    private Date createdAt;
+    private boolean isSeen;
 }
