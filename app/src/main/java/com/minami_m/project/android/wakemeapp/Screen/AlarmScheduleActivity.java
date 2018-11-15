6ed370bd4fe7e09d98a8f6af7277bbc06f8e7321
Minami Munakata <june28munazo@gmail.com>
2018-11-14 20:47:14 -0800

    create an AlarmScheduleActivity

---
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/AlarmScheduleActivity.java
@@ -0,0 +1,15 @@
+package com.minami_m.project.android.wakemeapp.Screen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.minami_m.project.android.wakemeapp.R;
+
+public class AlarmScheduleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm_schedule);
+    }
+}
