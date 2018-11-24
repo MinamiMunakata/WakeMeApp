801627ef4516c00be683c8305fa687d59f181ab7
Minami Munakata <june28munazo@gmail.com>
2018-11-24 14:50:16 -0800

    create Alarm Activity

--- app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/AlarmActivity.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/AlarmActivity.java
@@ -1,4 +1,13 @@
 package com.minami_m.project.android.wakemeapp.Screen;
 
-public class AlarmActivity {
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlarmActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alarm);
+    }
 }
