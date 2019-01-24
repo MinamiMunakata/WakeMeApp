8799e0a66252c50a48fe25c542b29117094ad628
Minami Munakata <june28munazo@gmail.com>
2019-01-24 13:55:40 -0800

    Create a BroadCastReceiver

--- app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/Alarm/AlarmActivity.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/Alarm/AlarmActivity.java
@@ -2,6 +2,7 @@ package com.minami_m.project.android.wakemeapp.Screen.Alarm;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.graphics.Paint;
@@ -53,8 +54,6 @@ public class AlarmActivity extends AppCompatActivity implements ActivityChangeLi
     private static final int DEFAULT_MINUTE = 0;
     private View[] optionButtons = new View[7];
     private Button alarmButton;
-    private static final String CHANNEL_ID = "alarm";
-    private static final int NOTIFICATION_ID = 100;
 
 
     @Override
@@ -187,36 +186,9 @@ public class AlarmActivity extends AppCompatActivity implements ActivityChangeLi
 //                    mustWakeUpSwitch.setChecked(true);
 //                }
 //                createAlarm(wakeUpTime);
-                NotificationCompat.Builder builder =
-                        new NotificationCompat.Builder(getApplicationContext(), CHANNEL_ID)
-                        .setContentTitle("WakeMeApp")
-                                .setSmallIcon(R.drawable.ico_alart)
-                        .setContentText("Much longer text that cannot fit one line...")
-                        .setStyle(new NotificationCompat.BigTextStyle()
-                                .bigText("Much longer text that cannot fit one line..."))
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-                NotificationManagerCompat managerCompat = NotificationManagerCompat.from(getApplicationContext());
-                createNotificationChannel();
-                managerCompat.notify(NOTIFICATION_ID, builder.build());
 
             }
         });
-    }
-
-    private void createNotificationChannel() {
-        // Create the NotificationChannel, but only on API 26+ because
-        // the NotificationChannel class is new and not in the support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = getString(R.string.app_name);
-            String description = getString(R.string.app_name);
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
-            channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
-            NotificationManager notificationManager = getSystemService(NotificationManager.class);
-            notificationManager.createNotificationChannel(channel);
-        }
     }
 
     private CompoundButton.OnCheckedChangeListener toggleMustWakeUp() {
