61880a36cef7e0bd28deca71f87f7b83affdc3c2
Minami Munakata <june28munazo@gmail.com>
2019-01-26 19:38:18 -0800

    set multiple notification.

--- app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/Alarm/NotificationController.java
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/Screen/Alarm/NotificationController.java
@@ -5,11 +5,14 @@ import android.app.Notification;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.util.Log;
 
 import com.minami_m.project.android.wakemeapp.Model.WakeUpTime;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Locale;
 
 import static android.content.Context.ALARM_SERVICE;
 
@@ -22,70 +25,75 @@ public class NotificationController {
         return new NotificationController(context);
     }
 
-    public NotificationController() {
-    }
-
-    public NotificationController(Context context) {
+    private NotificationController(Context context) {
         this.context = context;
     }
-//
-//    if (hourOfDay >= 12) {
-//        AM_PM = "PM";
-//        if (hourOfDay >= 13) {
-//            hourOfDay -= 12;
-//        }
-//    } else if (hourOfDay == 0) {
-//        hourOfDay = 12;
-//    } else if (hourOfDay < 10) {
-//        zeroHour = "0";
-//    }
-//        if (minute < 10) {
-//        zeroMin = "0";
-//    }
+
     public void setNotification(WakeUpTime wakeUpTime) {
-        // Set Time
+        if (wakeUpTime.getMustWakeUp()) {
+            if (wakeUpTime.getRepeatIsOn()) {
+                ArrayList<Integer> extraDays = wakeUpTime.extraDays();
+                for (Integer day: extraDays) {
+                    // Create time
+                    Calendar time = generateWakeUpTime(wakeUpTime);
+                    // Search for coming day of week
+                    while (time.get(Calendar.DAY_OF_WEEK) != day) {
+                        time.add(Calendar.DATE, 1);
+                    }
+                    // Create Intent
+                    PendingIntent sender = generateSender(day, PendingIntent.FLAG_UPDATE_CURRENT);
+                    // Let know AlarmManager
+                    AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+                    alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), INTERVAL_WEEK, sender);
+                    /* TODO: Canceling */
+                    alarmManager.cancel(sender);
+                    sender.cancel();
+                }
+
+            } else {
+                // Set Time
+                Calendar time = generateWakeUpTime(wakeUpTime);
+                // Create Intent
+                PendingIntent sender = generateSender(REQUEST_CODE_ONCE, PendingIntent.FLAG_ONE_SHOT);
+                // Let know AlarmManager
+                AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
+                alarmManager.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), sender);
+                /* TODO: Canceling */
+                alarmManager.cancel(sender);
+                sender.cancel();
+
+            }
+        }
+        // TODO: Remove it later
+        //checking if alarm is working with pendingIntent
+        for (int requestCode = 0; requestCode <= 7; requestCode++) {
+            PendingIntent sender = generateSender(requestCode, PendingIntent.FLAG_NO_CREATE);
+
+            boolean isWorking = (sender != null);//just changed the flag
+            Log.d("Notification Controller", "boolean:" + isWorking);
+            Log.d("Notification Controller", "Request Code: " + requestCode);
+            Log.d("Notification Controller", "alarm is " + (isWorking ? "" : "not") + " working...");
+        }
+
+    }
+
+    private PendingIntent generateSender(Integer requestCode, int flagUpdateCurrent) {
+        Intent intent = new Intent(context, NotificationReceiver.class);
+        return PendingIntent.getBroadcast(
+                context,
+                requestCode,
+                intent,
+                flagUpdateCurrent);
+    }
+
+    @NonNull
+    private Calendar generateWakeUpTime(WakeUpTime wakeUpTime) {
         Calendar time = Calendar.getInstance();
-        time.set(Calendar.HOUR, wakeUpTime.getHourOfDay());
+        time.set(Calendar.HOUR_OF_DAY, wakeUpTime.getHourOfDay());
         time.set(Calendar.MINUTE, wakeUpTime.getMinute());
         if (time.getTimeInMillis() <= Calendar.getInstance().getTimeInMillis()) {
             time.add(Calendar.DATE, 1); // Tomorrow
         }
-        if (wakeUpTime.getMustWakeUp()) {
-            if (wakeUpTime.getRepeatIsOn()) {
-                ArrayList<Integer> extraDays = wakeUpTime.extraDays();
-
-                for (Integer day: extraDays) {
-                    // TODO: set all alarm
-                    System.out.println(day);
-                }
-//
-//                // Create Intent
-//                Intent intent = new Intent(context, NotificationReceiver.class);
-//                PendingIntent sender = PendingIntent.getBroadcast(
-//                        context,
-//                        REQUEST_CODE_ONCE,
-//                        intent,
-//                        0);
-//                // Let know AlarmManager
-//                AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-//                alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), INTERVAL_WEEK, sender);
-            } else {
-                System.out.println("Going to send");
-                // Create Intent
-                Intent intent = new Intent(context, NotificationReceiver.class);
-                PendingIntent sender = PendingIntent.getBroadcast(
-                        context,
-                        REQUEST_CODE_ONCE,
-                        intent,
-                        0);
-                // Let know AlarmManager
-                AlarmManager alarmManager = (AlarmManager) context.getSystemService(ALARM_SERVICE);
-                alarmManager.set(AlarmManager.RTC_WAKEUP, time.getTimeInMillis(), sender);
-            }
-        }
-
-
-
-
+        return time;
     }
 }
