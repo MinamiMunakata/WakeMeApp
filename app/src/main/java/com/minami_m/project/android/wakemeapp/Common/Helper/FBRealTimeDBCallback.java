e79d650c4b23f1734d84908dc1c1e445c15afbf1
Minami Munakata <june28munazo@gmail.com>
2019-02-15 20:43:55 -0800

    Change a class's name

---
+++ app/src/main/java/com/minami_m/project/android/wakemeapp/Common/Helper/FBRealTimeDBCallback.java
@@ -0,0 +1,7 @@
+package com.minami_m.project.android.wakemeapp.Common.Helper;
+
+import com.minami_m.project.android.wakemeapp.Model.User;
+
+public interface FBRealTimeDBCallback {
+    void retrieveUserData(User user);
+}
