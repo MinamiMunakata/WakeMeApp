8a6a6a95cfaa920a2c055272be4695536cebb908
Minami Munakata <june28munazo@gmail.com>
2019-02-23 13:40:57 -0800

    modify notification.

--- functions/index.js
+++ functions/index.js
@@ -5,32 +5,32 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Take the text parameter passed to this HTTP endpoint and insert it into the
-// Realtime Database under the path functions/messages/:pushId/original
-exports.addMessage = functions.https.onRequest((req, res) => {
-    // Grab the text parameter.
-    const original = req.query.text;
-    // Push the new message into the Realtime Database using the Firebase Admin SDK.
-    return admin.database().ref('functions/messages').push({original: original}).then((snapshot) => {
-        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
-        return res.redirect(303, snapshot.ref.toString());
-    });
-});
-
-// Listens for new messages added to /functions/messages/:pushId/original and creates an
-// uppercase version of the message to /functions/messages/:pushId/uppercase
-exports.makeUppercase = functions.database.ref('/functions/messages/{pushId}/original')
-    .onCreate((snapshot, context) => {
-        const original = snapshot.val();
-        console.log('Uppercasing', context.params.pushId, original);
-        const uppercase = original.toUpperCase();
-        // You must return a Promise when performing asynchronous tasks inside a Functions such as
-        // writing to the Firebase Realtime Database.
-        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
-        return snapshot.ref.parent.child('uppercase').set(uppercase)
-    });
-
-// ------------------------
+//// Take the text parameter passed to this HTTP endpoint and insert it into the
+//// Realtime Database under the path functions/messages/:pushId/original
+//exports.addMessage = functions.https.onRequest((req, res) => {
+//    // Grab the text parameter.
+//    const original = req.query.text;
+//    // Push the new message into the Realtime Database using the Firebase Admin SDK.
+//    return admin.database().ref('functions/messages').push({original: original}).then((snapshot) => {
+//        // Redirect with 303 SEE OTHER to the URL of the pushed object in the Firebase console.
+//        return res.redirect(303, snapshot.ref.toString());
+//    });
+//});
+//
+//// Listens for new messages added to /functions/messages/:pushId/original and creates an
+//// uppercase version of the message to /functions/messages/:pushId/uppercase
+//exports.makeUppercase = functions.database.ref('/functions/messages/{pushId}/original')
+//    .onCreate((snapshot, context) => {
+//        const original = snapshot.val();
+//        console.log('Uppercasing', context.params.pushId, original);
+//        const uppercase = original.toUpperCase();
+//        // You must return a Promise when performing asynchronous tasks inside a Functions such as
+//        // writing to the Firebase Realtime Database.
+//        // Setting an "uppercase" sibling in the Realtime Database returns a Promise.
+//        return snapshot.ref.parent.child('uppercase').set(uppercase)
+//    });
+//
+//// ------------------------
 /*
     TODO:
     1. Observe firebase realtime database if a new message is added.
@@ -41,18 +41,21 @@ exports.makeUppercase = functions.database.ref('/functions/messages/{pushId}/ori
     TODO:
     1. Modify architecture. You may need a notification node in realtime database.
 */
-exports.sendNotification = functions.database.ref('/functions/messages/{pushId}/original')
+exports.sendNotification = functions.database.ref('/Messages/{chatRoomId}/{pushId}')
     .onCreate((snap, context) => {
-//        const val = event.data.val();
-//        console.log('helloWorld', val);
-        console.log('helloWorld');
+        const val = snap.val();
+        console.log('snap', val);
+        const text = val.text;
+        const senderId = val.senderId;
+
+        console.log('chatRoomId', context.params.chatRoomId);
         // The topic name can be optionally prefixed with "/topics/".
         var topic = 'messages';
 
         var payload = {
             notification: {
-            title: "新着メッセージ", // この辺りを動的に取ってくるケースが多いかな？
-            body: "新着メッセージがあります"
+            title: "You've gotta mail!",
+            body: text
             },
             topic: topic
         };
